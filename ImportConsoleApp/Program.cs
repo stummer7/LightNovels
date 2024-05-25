d496e41231faa1bd80aecf95a698d42ddbc7c8c5
Benedikt Stummer <55189345+stummer7@users.noreply.github.com>
2024-05-25 21:02:08 +0200

    WebScrapper init

--- ImportConsoleApp/Program.cs
+++ ImportConsoleApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Persistence;
-using HtmlAgilityPack;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Core.Entities;
 
 //Console.WriteLine("Import der Movies und Categories in die Datenbank");
 //await using var unitOfWork = new UnitOfWork();
@@ -17,3 +19,30 @@ using HtmlAgilityPack;
 //Console.Write("Beenden mit Eingabetaste ...");
 //Console.ReadLine();
 
+string url = "https://www.lightnovelcave.com/browse/genre-all-25060123/order-new/status-all";
+IList<Novel> novels = new List<Novel>();
+
+
+var chromeOptions = new ChromeOptions();
+chromeOptions.AddArguments("headless");
+var driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl(url);
+
+Thread.Sleep(5000);
+
+IWebElement card = driver.FindElement(By.XPath("/html/body/div[1]/div/div/div/div[2]/div/button[2]"));
+card.Click();
+
+IList<IWebElement> elements = driver.FindElements(By.ClassName("novel-item"));
+
+foreach (var item in elements)
+{
+    IList<IWebElement> links = item.FindElements(By.TagName("a"));
+    IWebElement link = links.First();
+    Console.WriteLine(link.Text);
+    link.Click();
+}
+
+
+//driver.Quit();
